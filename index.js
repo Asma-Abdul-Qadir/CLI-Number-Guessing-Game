32832a30925fd6c7ca3100dc7ce8b56ebc895df4
Asma-Abdul-Qadir <asmagigani786@gmail.com>
2024-05-30 23:58:44 +0500

    created number guessing game

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\tWelcome to the CLI Number Guessing Game\n");
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([{
+        name: "Userguessnumber",
+        type: "number",
+        message: "Please guess a number between 1-10:",
+    },
+]);
+if (answer.userguessednumber === randomNumber) {
+    console.log("Congratulation ! you guess a right number.");
+}
+else {
+    console.log("Sorry, you guess a wrong number try again");
+}
